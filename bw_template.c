609eba0eb1979844b632d4b85dc8d61e269e4c26
KerenSfez2304 <94888143+KerenSfez2304@users.noreply.github.com>
2023-08-27 18:24:01 +0300

    ex3

--- bw_template.c
+++ bw_template.c
@@ -1491,7 +1491,9 @@ int run_server (struct pingpong_context *clients_ctx[NUM_CLIENT])
       for (int i = 0; i < NUM_CLIENT; i++)
         {
           struct ibv_wc wc[WC_BATCH];
+          printf("Before ibv\n");
           int ne = ibv_poll_cq (clients_ctx[i]->cq, WC_BATCH, wc);
+          printf("After ibv\n");
 
           if (ne < 0)
             {
