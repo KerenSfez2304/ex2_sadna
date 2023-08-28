cfb5f8e7d08523e005fc9542d771d8f70481ed97
KerenSfez2304 <94888143+KerenSfez2304@users.noreply.github.com>
2023-08-28 18:17:26 +0300

    ex3

--- bw_template.c
+++ bw_template.c
@@ -1523,12 +1523,12 @@ int run_server (struct pingpong_context *clients_ctx[NUM_CLIENT])
 //                  curr_ = clients_ctx[i]->buf[clients_ctx[i]->currBuffer];
                   curr_ = clients_ctx[i]->buf[clients_ctx[i]->currBuffer - 1];
                   printf("%c %c key: %s  value: %s    buffer: %d\n", curr_->request_type, curr_->protocol, curr_->key, curr_->value, clients_ctx[i]->currBuffer-1);
-                  curr_ = clients_ctx[i]->buf[clients_ctx[i]->currBuffer + 1];
+                  curr_ = clients_ctx[i]->buf[clients_ctx[i]->currBuffer];
                   printf("%c %c key: %s  value: %s    buffer: %d\n", curr_->request_type, curr_->protocol, curr_->key, curr_->value, clients_ctx[i]->currBuffer);
                   curr_ = clients_ctx[i]->buf[clients_ctx[i]->currBuffer + 1];
                   printf("%c %c key: %s  value: %s    buffer: %d\n", curr_->request_type, curr_->protocol, curr_->key, curr_->value, clients_ctx[i]->currBuffer+1);
                   fflush(stdout);
-                  curr_ = clients_ctx[i]->buf[clients_ctx[i]->currBuffer - 1];
+                  curr_ = clients_ctx[i]->buf[clients_ctx[i]->currBuffer];
                 } else {
             printf("%c %c key: %s  value: %s    buffer: %d\n", curr_->request_type, curr_->protocol, curr_->key, curr_->value, clients_ctx[i]->currBuffer);
             fflush(stdout);
