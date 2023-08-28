ac3950dd71e98f64e2586c6580d2555fe836f22e
KerenSfez2304 <94888143+KerenSfez2304@users.noreply.github.com>
2023-08-28 17:44:15 +0300

    ex3

--- bw_template.c
+++ bw_template.c
@@ -1319,7 +1319,8 @@ int kv_rdv_set (struct pingpong_context *ctx, struct packet *packet, const char 
   /* Send FIN message */
   ctx->size = 1;
   packet->request_type = 'f';
-  printf("sending FIN for key: %s    value: %s\n", packet->key, packet->value);
+  struct packet* fin_packet = ctx->buf[ctx->currBuffer];
+  printf("sending FIN for key: %s    value: %s\n", fin_packet->key, fin_packet->value);
   fflush(stdout);
   pp_post_send (ctx, NULL, NULL, 0, IBV_WR_SEND);
   pp_wait_completions (ctx, 1);
